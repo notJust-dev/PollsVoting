dc6b1507faf69cb715e3ad237487c9ac86a8291a
Vadim Savin <savinvadim1312@gmail.com>
2024-04-23 18:20:33 +0100

    Create polls

--- src/app/polls/new.tsx
+++ src/app/polls/new.tsx
@@ -1,16 +1,39 @@
-import { Redirect, Stack } from 'expo-router';
+import { Redirect, Stack, router } from 'expo-router';
 import { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button, Alert } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { useAuth } from '../../providers/AuthProvider';
+import { supabase } from '../../lib/supabase';
 
 export default function CreatePoll() {
   const [question, setQuestion] = useState('');
   const [options, setOptions] = useState(['', '']);
+  const [error, setError] = useState('');
 
   const { user } = useAuth();
 
-  const createPoll = () => {
+  const createPoll = async () => {
+    setError('');
+    if (!question) {
+      setError('Please provide the question');
+      return;
+    }
+    const validOptions = options.filter((o) => !!o);
+    if (validOptions.length < 2) {
+      setError('Please provide at least 2 valid options');
+      return;
+    }
+
+    const { data, error } = await supabase
+      .from('polls')
+      .insert([{ question, options: validOptions }])
+      .select();
+    if (error) {
+      Alert.alert('Failed to create the poll');
+      console.log(error);
+      return;
+    }
+    router.back();
     console.warn('Create');
   };
 
@@ -60,6 +83,7 @@ export default function CreatePoll() {
       <Button title="Add option" onPress={() => setOptions([...options, ''])} />
 
       <Button title="Create poll" onPress={createPoll} />
+      <Text style={{ color: 'crimson' }}>{error}</Text>
     </View>
   );
 }
