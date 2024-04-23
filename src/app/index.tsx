8a97b3fd73db994fc0a91b634c421a1fc8ee7747
Vadim Savin <savinvadim1312@gmail.com>
2024-04-23 15:44:43 +0100

    home screen: polls list

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Stack } from 'expo-router';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+const polls = [1, 2, 3];
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Stack.Screen options={{ title: 'Polls' }} />
+      <FlatList
+        data={polls}
+        contentContainerStyle={styles.container}
+        renderItem={() => (
+          <View style={styles.pollContainer}>
+            <Text style={styles.pollTitle}>Example poll question</Text>
+          </View>
+        )}
+      />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    gap: 5,
+  },
+  pollContainer: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
+  },
+  pollTitle: {
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
